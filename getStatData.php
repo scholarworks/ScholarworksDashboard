63e67f2cb09a159854316b8271793cf28a3ca472
Aaron Collier <acollier@calstate.edu>
2014-03-28 15:00:56 -0700

    no message

--- getStatData.php
+++ getStatData.php
@@ -10,3 +10,6 @@ $data_url = "http://localhost:24091/solr/search/select/?q=*:*&wt=json&rows=0&&fa
 $json_array = json_decode(file_get_contents($data_url),TRUE);
 print_r($json_array);
 
+echo "--------------- <br /> -------------------- <br /> <br />";
+
+print_r($json_array[facet_counts][facet_fields][type_keyword]);
