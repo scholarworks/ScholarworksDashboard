b1b3e8ecdf8534af5974a82a63ebba9233f8cdd1
Aaron Collier <acollier@calstate.edu>
2014-03-28 14:58:04 -0700

    no message

--- getStatData.php
+++ getStatData.php
@@ -3,13 +3,10 @@
 if($_GET["facet"] === "") echo "Facet String is Empty\n"; // TODO: Determin need for default
 
 $facet = $_GET["facet"];
-$data_url = "http://localhost:24091/solr/search/select/?q=*%3A*&version=2.2&start=0&rows=0&indent=on&facet=true&facet.field=$facet"; // returns JSON formatted facet counts from solr.
+$data_url = "http://localhost:24091/solr/search/select/?q=*:*&wt=json&rows=0&&facet=true&facet.field=$facet"; // returns JSON formatted facet counts from solr.
 
 // echo file_get_contents($data_url);
 
-// $json_array = json_decode(file_get_contents($data_url),TRUE);
-// print_r($json_array);
+$json_array = json_decode(file_get_contents($data_url),TRUE);
+print_r($json_array);
 
-$xml = simplexml_load_file($data_url);
-
-print_r($xml);
