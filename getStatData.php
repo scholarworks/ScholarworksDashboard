76f31015ac54b0c1e6316e3bf59ffcb61b25674c
Aaron Collier <acollier@calstate.edu>
2014-03-28 15:18:54 -0700

    Outputting JSON from php for SOLR facet counts

--- getStatData.php
+++ getStatData.php
@@ -10,20 +10,21 @@ $data_url = "http://localhost:24091/solr/search/select/?q=*:*&wt=json&rows=0&&fa
 $json_array = json_decode(file_get_contents($data_url),TRUE);
 // print_r($json_array);
 
-echo "--------------- <br /> -------------------- <br /> <br />";
+// echo "--------------- <br /> -------------------- <br /> <br />";
 
 // print_r($json_array[facet_counts][facet_fields][type_keyword]);
 $array_for_loop = $json_array[facet_counts][facet_fields][$facet];
 
 // print_r($array_for_loop);
 $array_for_json = array();
-echo "COUNT = " . count($array_for_loop);
+// echo "COUNT = " . count($array_for_loop);
 
 for($i=0; $i<count($array_for_loop); $i+=2) {
 	$step = $i;
 	$step2 = $i+1;
-	echo $array_for_loop[$step] . " => " . $array_for_loop[$step2] . "<br />";
+	// echo $array_for_loop[$step] . " => " . $array_for_loop[$step2] . "<br />";
 	array_push($array_for_json,array("name" => $array_for_loop[$step], "count" => $array_for_loop[$step2]));
 }
 
-print_r($array_for_json);
+echo json_encode($array_for_json);
+// print_r($array_for_json);
