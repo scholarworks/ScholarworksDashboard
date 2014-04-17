b1aef4a50b9fe4f4d843b9b299b9b8985997a453
Aaron Collier <acollier@calstate.edu>
2014-04-17 12:51:31 -0700

    no message

--- getStatData.php
+++ getStatData.php
@@ -28,8 +28,9 @@ for($i=0; $i<count($array_for_loop); $i+=2) {
 	$nextLevelDataUrl = "http://localhost:24091/solr/search/select/?q=$facet:\"$name\"&wt=json&rows=0&&facet=true&facet.field=$facet"; // returns JSON formatted facet counts from solr.
 	$nextLevelJsonArray = json_decode(file_get_contents($nextLevelDataUrl),TRUE);
 
+	echo "NAME = $name\n\n";
 	print_r($nextLevelJsonArray);
-	
+	echo "\n\n********************************\n\n";
 	// echo $array_for_loop[$step] . " => " . $array_for_loop[$step2] . "<br />";
 	array_push($array_for_json,array("name" => $array_for_loop[$step], "count" => $array_for_loop[$step2]));
 }
